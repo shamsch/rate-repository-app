afffd61d4754b30dba68d64f7e03c537d3053e7d
Shamsur Raza Chowdhury <shamsur314@gmail.com>
2022-06-15 23:12:28 +0300

    ex 10.5

--- src/component/RepositoryItem .jsx
+++ src/component/RepositoryItem .jsx
@@ -1,16 +1,112 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 import React from "react";
+import millify from "millify";
+
+const styles = StyleSheet.create({
+    container: {
+        margin: 10,
+        backgroundColor: "lightgray",
+        borderRadius: 10,
+        padding: 10,
+    },
+    top: {
+        flexDirection: "row",
+        alignItems: "center",
+        padding: 5,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: "bold",
+    },
+    subtitle: {
+        fontSize: 14,
+        color: "gray",
+    },
+    tag: {
+        fontSize: 14,
+        color: "black",
+        fontWeight: "bold",
+        backgroundColor: "lightblue",
+        padding: 2,
+        borderRadius: 5,
+        marginLeft: 5,
+    },
+    logo: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        marginRight: 10,
+    },
+    box: {
+        height: 50,
+        width: 60,
+        marginHorizontal: 15,
+        marginTop: 10,
+    },
+    count: {
+        fontSize: 14,
+        fontWeight: "bold",
+        color: "black",
+        textAlign: "center",
+    },
+    boxText: {
+        fontSize: 14,
+        color: "white",
+        fontWeight: "bold",
+        marginLeft: 5,
+        textAlign: "center",
+    },
+});
 
 const RepositoryItem = ({ item }) => {
     return (
-        <View>
-            <Text>Full name: {item.fullName}</Text>
-            <Text>Description: {item.description}</Text>
-            <Text>Language: {item.language}</Text>
-            <Text>Stars: {item.stargazersCount}</Text>
-            <Text>Forks: {item.forksCount}</Text>
+        <View style={styles.container}>
+            <View style={styles.top}>
+                <Image
+                    style={styles.logo}
+                    source={{
+                        uri: `${item.ownerAvatarUrl}`,
+                    }}
+                />
+                {/* this flex shrink makes the text stay within the view which is within the the top view */}
+                <View style={{ flexShrink: 1 }}> 
+                    <Text style={styles.title}>{item.fullName}</Text>
+                    <Text style={styles.subtitle}>{item.description}</Text>
+                </View>
+            </View>
+
+            <View style={{ alignSelf: "flex-start", marginLeft: 60 }}>
+                <Text style={styles.tag}>{item.language}</Text>
+            </View>
+
+            <View style={{ flexDirection: "row" }}>
+                <View style={styles.box}>
+                    <Text style={styles.count}>
+                        {millify(item.stargazersCount)}
+                    </Text>
+                    <Text style={styles.boxText}>Stars</Text>
+                </View>
+                <View style={styles.box}>
+                    <Text style={styles.count}>{millify(item.forksCount)}</Text>
+                    <Text style={styles.boxText}>Forks</Text>
+                </View>
+                <View style={styles.box}>
+                    <Text style={styles.count}>
+                        {millify(item.reviewCount)}
+                    </Text>
+                    <Text style={styles.boxText}>Reviews</Text>
+                </View>
+                <View style={styles.box}>
+                    <Text style={styles.count}>
+                        {millify(item.ratingAverage)}
+                    </Text>
+                    <Text style={styles.boxText}>Rating</Text>
+                </View>
+            </View>
+
+            {/* <Text>Forks: </Text>
             <Text>Reviews: {item.reviewCount}</Text>
-            <Text>Rating: {item.ratingAverage}</Text>
+            <Text>Rating: {item.ratingAverage}</Text> */}
         </View>
     );
 };
