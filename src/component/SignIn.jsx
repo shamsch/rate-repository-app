5e5a60e1e8ce0e7298fcf7c1cdc6f1b9bcaeb2d4
Shamsur Raza Chowdhury <shamsur314@gmail.com>
2022-06-18 19:00:51 +0300

    10.8

--- src/component/SignIn.jsx
+++ src/component/SignIn.jsx
@@ -1,10 +1,54 @@
-import { Text, View } from "react-native";
+import { TextInput, Button, View, StyleSheet } from "react-native";
+import { Formik } from "formik";
+
+const styles = StyleSheet.create({
+    input: {
+        height: 40,
+        borderColor: "gray",
+        borderWidth: 1,
+        margin: 10,
+        borderRadius: 5,
+        padding: 5,
+    },
+});
+
 const SignIn = () => {
-  return (
-    <View>
-        <Text>Login</Text>
-    </View>
-  );
+    return (
+        <View>
+            <Formik
+                initialValues={{ username: "", password: "" }}
+                onSubmit={(values) => {
+                    console.log(values);
+                }}
+            >
+                {(props) => (
+                    <View>
+                        <TextInput
+                            style={styles.input}
+                            placeholder="username"
+                            onChangeText={props.handleChange("username")}
+                            value={props.values.username}
+                        />
+
+                        <TextInput
+                            style={styles.input}
+                            secureTextEntry
+                            placeholder="password"
+                            onChangeText={props.handleChange("password")}
+                            value={props.values.password}
+                        />
+
+                        <Button
+                            color={"#00BFFF"}
+                            style={styles.submitBtn}
+                            title="Submit"
+                            onPress={props.handleSubmit}
+                        />
+                    </View>
+                )}
+            </Formik>
+        </View>
+    );
 };
 
 export default SignIn;
