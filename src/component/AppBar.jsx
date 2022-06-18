a62814eb5519849b25b24c04383bf3d1faeeb3f0
Shamsur Raza Chowdhury <shamsur314@gmail.com>
2022-06-18 16:13:27 +0300

    10.5

--- src/component/AppBar.jsx
+++ src/component/AppBar.jsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, Pressable } from "react-native";
+import { View, Text, StyleSheet, Pressable, ScrollView } from "react-native";
 import React from "react";
 import Constants from "expo-constants";
 import { Link } from "react-router-native";
@@ -8,25 +8,33 @@ const styles = StyleSheet.create({
         paddingTop: Constants.statusBarHeight,
         backgroundColor: "#00BFFF",
         padding: 10,
+        flexDirection: "row",
     },
     text: {
         color: "white",
         fontSize: 20,
         fontWeight: "bold",
     },
+    item: {
+        marginRight: 10,
+    },
 });
 
 const AppBar = () => {
     return (
         <View style={styles.container}>
-            <Pressable>
-                <Text style={styles.text}>Repositories</Text>
-            </Pressable>
-            <Pressable>
-                <Link to="/login">
-                    <Text style={styles.text}>Sign In</Text>
-                </Link>
-            </Pressable>
+            <ScrollView horizontal>
+                <Pressable style={styles.item}>
+                    <Link to="/">
+                        <Text style={styles.text}>Repositories</Text>
+                    </Link>
+                </Pressable>
+                <Pressable style={styles.item}>
+                    <Link to="/login">
+                        <Text style={styles.text}>Sign In</Text>
+                    </Link>
+                </Pressable>
+            </ScrollView>
         </View>
     );
 };
