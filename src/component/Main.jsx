32335784f19c8c7d9a30e1ebc51ff68e6254d3c8
Shamsur Raza Chowdhury <shamsur314@gmail.com>
2022-06-19 17:09:27 +0300

    10.10

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -1,12 +1,13 @@
 import Constants from "expo-constants";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Platform } from "react-native";
 import AppBar from "./AppBar";
 import RepositoryList from "./RepositoryList ";
-import { Route, Routes, Navigate } from "react-router-native";
+import { Route, Routes, Navigate} from "react-router-native";
 import SignIn from "./SignIn";
 
 const styles = StyleSheet.create({
     container: {
+        fontFamily: Platform.OS === "ios" ? "Arial" : "Roboto",
         marginTop: Constants.statusBarHeight,
         flexGrow: 1,
         flexShrink: 1,
