924af465e9a950acc337a7c7f4b9b4206e7d93b3
Shamsur Chowdhury <shamsur.chowdhury@crowdsorsa.com>
2022-06-22 15:41:27 +0300

    10.11

--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -1,27 +1,17 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import { useQuery } from "@apollo/client";
+import { GET_REPOSITORIES } from "../graphql/queries";
 
 const useRepositories = () => {
-    const [repositories, setRepositories] = useState();
-    const [loading, setLoading] = useState(false);
+	const res = useQuery(GET_REPOSITORIES, {
+		fetchPolicy: "cache-and-network",
+	});
 
-    const fetchRepositories = async () => {
-        setLoading(true);
+	if (res.loading) {
+		return { loading: true };
+	}
 
-        // Replace the IP address part with your own IP address!
-        const response = await fetch(
-            "http://192.168.2.100:5000/api/repositories"
-        );
-        const json = await response.json();
-
-        setLoading(false);
-        setRepositories(json);
-    };
-
-    useEffect(() => {
-        fetchRepositories();
-    }, []);
-
-    return { repositories, loading, refetch: fetchRepositories };
+	return { repositories: res.data.repositories, loading: false };
 };
 
 export default useRepositories;
