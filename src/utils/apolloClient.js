15d8965be74b16d3a6e72ae208e1b39723e3c8d1
Shamsur Chowdhury <shamsur.chowdhury@crowdsorsa.com>
2022-06-23 11:52:55 +0300

    10.12

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,8 +1,12 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 
+import Constants from "expo-constants";
+
+export const uri = Constants.manifest.extra.uri;
+
 const httpLink = createHttpLink({
 	// Replace the IP address part with your own IP address!
-	uri: "http://192.168.3.5:4000/graphql",
+	uri: uri,
 });
 
 const createApolloClient = () => {
