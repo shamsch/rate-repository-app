c10a2cb0fd88ae0b5b98ad2651d785b6959845fe
Shamsur Chowdhury <shamsur.chowdhury@crowdsorsa.com>
2022-07-05 14:59:06 +0300

    added fetchmore

--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -3,39 +3,59 @@ import { useQuery } from "@apollo/client";
 import { GET_REPOSITORIES } from "../graphql/queries";
 
 const useRepositories = (selected, debouncedSearch) => {
-    let variables = {};
-	
+	let variables = {};
+
 	//clearly i don't understand what ascend and descend mean but it works lol
-    if (selected === "ASC") {
-        variables = {
-            orderBy: "RATING_AVERAGE",
-            orderDirection: "DESC",
-			searchKeyword: debouncedSearch? debouncedSearch : ""
-        };
-    } else if (selected === "DESC") {
-        variables = {
-            orderBy: "RATING_AVERAGE",
-            orderDirection: "ASC",
-			searchKeyword: debouncedSearch? debouncedSearch : ""
-        };
-    } else if (selected === "LATEST") {
+	if (selected === "ASC") {
+		variables = {
+			orderBy: "RATING_AVERAGE",
+			orderDirection: "DESC",
+			searchKeyword: debouncedSearch ? debouncedSearch : "",
+		};
+	} else if (selected === "DESC") {
+		variables = {
+			orderBy: "RATING_AVERAGE",
+			orderDirection: "ASC",
+			searchKeyword: debouncedSearch ? debouncedSearch : "",
+		};
+	} else if (selected === "LATEST") {
 		variables = {
 			orderBy: "CREATED_AT",
 			orderDirection: "ASC",
-			searchKeyword: debouncedSearch? debouncedSearch : ""
+			searchKeyword: debouncedSearch ? debouncedSearch : "",
 		};
 	}
 
-    const res = useQuery(GET_REPOSITORIES, {
-        fetchPolicy: "cache-and-network",
-        variables,
-    });
+	const { data, loading, fetchMore, ...result } = useQuery(GET_REPOSITORIES, {
+		fetchPolicy: "cache-and-network",
+		variables,
+	});
 
-    if (res.loading) {
-        return { loading: true };
-    }
-    
-    return { repositories: res.data.repositories, loading: false };
+	const handleFetchMore = () => {
+		const canFetchMore = !loading && data?.repositories.pageInfo.hasNextPage;
+
+		if (!canFetchMore) {
+			return;
+		}
+
+		fetchMore({
+			variables: {
+				after: data.repositories.pageInfo.endCursor,
+				...variables,
+			},
+		});
+	};
+
+	if (loading) {
+		return { loading: true };
+	}
+
+	return {
+		repositories: data?.repositories,
+		loading: false,
+		fetchMore: handleFetchMore,
+		...result,
+	};
 };
 
 export default useRepositories;
--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,12 +1,25 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
-
+import { relayStylePagination } from "@apollo/client/utilities";
 import Constants from "expo-constants";
 
-import { setContext } from '@apollo/client/link/context';
+import { setContext } from "@apollo/client/link/context";
 
 export const uri = Constants.manifest.extra.uri;
 
-console.log(uri);
+//To describe Apollo Client, how to merge the existing repositories in the cache with the next set of repositories, we can use a field policy.
+//In general, field policies can be used to customize the cache behavior during read and write operations with read and merge functions.
+//As mentioned earlier, the format of the pagination's result object and the arguments are based on the Relay's pagination specification.
+//Luckily, Apollo Client provides a predefined field policy, relayStylePagination, which can be used in this case.
+
+const cache = new InMemoryCache({
+	typePolicies: {
+		Query: {
+			fields: {
+				repositories: relayStylePagination(),
+			},
+		},
+	},
+});
 
 const httpLink = createHttpLink({
 	// Replace the IP address part with your own IP address!
@@ -16,24 +29,24 @@ const httpLink = createHttpLink({
 const createApolloClient = (authStorage) => {
 	const authLink = setContext(async (_, { headers }) => {
 		try {
-		  const accessToken = await authStorage.getAccessToken();
-		  return {
-			headers: {
-			  ...headers,
-			  authorization: accessToken ? `Bearer ${accessToken}` : '',
-			},
-		  };
+			const accessToken = await authStorage.getAccessToken();
+			return {
+				headers: {
+					...headers,
+					authorization: accessToken ? `Bearer ${accessToken}` : "",
+				},
+			};
 		} catch (e) {
-		  console.log(e);
-		  return {
-			headers,
-		  };
+			console.log(e);
+			return {
+				headers,
+			};
 		}
-	  });
-	  return new ApolloClient({
+	});
+	return new ApolloClient({
 		link: authLink.concat(httpLink),
-		cache: new InMemoryCache(),
-	  });
+		cache,
+	});
 };
 
 export default createApolloClient;
